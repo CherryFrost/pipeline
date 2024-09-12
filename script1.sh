ed798684200a018af8c24b8766b49e9a45025728
Cherry Frost <alhagues@gmail.com>
2024-09-12 09:05:43 -0700

    fixing broken build

--- script1.sh
+++ script1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "The Pipeline Works! Updating my configs. Sending confirmation via email. This should be a broken build and send an email."
+echo "The Pipeline Works! Updating my configs."
